a7ace991ec48a895d7ab4294ae15e01b98a9d0b4
Apple <apple@Apples-MacBook-Pro-10.local>
2019-07-18 11:08:46 -0700

    6

--- calc.js
+++ calc.js
@@ -1,11 +1,26 @@
 var button = $('button');
-
-
+var wastenumber = $('.wastenumber');
+var time = $('.freq').val();
+function freqConversion(time){
+  var freq = 0;
+  var freq = $('.freq').val();
+  if freq == "aweek" {
+    var freqNum = 52;
+  if
+}
 button.on('click', giveInfo);
-function giveInfo() {
-  var bot = $('#bot').val();
-  var str = $('#str').val();
-  var sho = $('#sho').val();
-  var dri = $('#dri').val();
 
+  var input = $('#numero').val();
+  var bot = $('#bot')
+  bot = 5;
+
+    freqNum * bot * input
+  }
+
+  // var str = $('#str').val();
+  // var sho = $('#sho').val();
+  // var dri = $('#dri').val();
+
+function giveInfo(freqConversion($'.')) {
+  wastenumber.append()
 }
