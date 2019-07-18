5a7625c093c3b75c0cee74c569ff7834241ba8be
Ava <avamarmor@Apples-MacBook-Pro-8.local>
2019-07-18 12:07:35 -0700

    commit before lunch

--- t-or-f.js
+++ t-or-f.js
@@ -8,6 +8,7 @@ function respondwithFalse(){
   event.preventDefault();
   console.log(truth);
   f.text(` You are incorrect. Around one million drinks that are in plastic bottles are sold every minute across the world. This is harmful because plastic cannot decompose, and not everybody recycles. Recycling would allow the plastic from the bottles to become repurposed and used again.  `);
+  truth.toggleClass(`red`);
 }
 
 myth.on("click",respondwithTrue);
@@ -16,6 +17,7 @@ function respondwithTrue(){
   event.preventDefault();
   console.log(myth);
   f.text(` You are correct. Around one million drinks that are in plastic bottles are sold every minute across the world. This is harmful because plastic cannot decompose, and not everybody recycles. Recycling would allow the plastic from the bottles to become repurposed and used again.  `);
+  myth.toggleClass(`green`);
 }
 
 var truth1 = $('.truth1');
@@ -27,6 +29,7 @@ truth1.on("click", respondwithTrue1);
 function respondwithTrue1() {
   event.preventDefault();
   false1.text(`You are correct. Unfortunately, at the rate and way we discard plastic will make the ocean have a great increase in plastic in the upcoming years.`);
+  truth1.toggleClass(`green`);
 }
 
 myth1.on("click", respondwithFalse1);
@@ -34,6 +37,7 @@ myth1.on("click", respondwithFalse1);
 function respondwithFalse1() {
   event.preventDefault();
   false1.text(`You are incorrect. Unfortunately, at the rate and way we discard plastic will make the ocean have a great increase in plastic in the upcoming years.`);
+  myth1.toggleClass(`red`);
 }
 
 var truth2 = $(".truth2");
@@ -45,6 +49,7 @@ truth2.on("click",truee);
 function truee() {
   event.preventDefault();
   false2.text(` You are correct. When synthetic fiber is rinsed or put under water, the fiber releases microplastics. Microplastics (or small pieces of plastic) can be mistaken for food by many sea creatures, such as sea turtles. These microplastics may release toxins into the creatures they are consumed by. If that creature is eaten by another creature, it will pass the toxins on the other creature, thus possibly disrupting food chains. Additionally, it sits in the stomach of creatures and make them feel full. This means that they will not be consuming food that they need. `);
+  truth2.toggleClass(`green`);
 }
 
 myth2.on("click", falsee);
@@ -52,6 +57,7 @@ myth2.on("click", falsee);
 function falsee() {
   event.preventDefault();
   false2.text(` You are incorrect. When synthetic fiber is rinsed or put under water, the fiber releases microplastics. Microplastics (or small pieces of plastic) can be mistaken for food by many sea creatures, such as sea turtles. These microplastics may release toxins into the creatures they are consumed by. If that creature is eaten by another creature, it will pass the toxins on the other creature, thus possibly disrupting food chains. Additionally, it sits in the stomach of creatures and make them feel full. This means that they will not be consuming food that they need. `);
+  myth2.toggleClass(`red`);
 }
 
 var truth3 = $(".truth3");
@@ -62,14 +68,16 @@ truth3.on("click", truth35);
 
 function truth35(){
   event.preventDefault();
-  false3.text(`You are correct.`);
+  false3.text(`You are correct. It is called bioplastic.`);
+  truth3.toggleClass(`green`);
 }
 
 myth3.on("click", myth35);
 
 function myth35(){
   event.preventDefault();
-  false3.text(`You are incorrect.`);
+  false3.text(`You are incorrect. it is called bioplastic.`);
+  myth3.toggleClass(`red`);
 }
 
 var truth4 = $(".truth4");
@@ -81,6 +89,7 @@ truth4.on("click", false45);
 function false45() {
   event.preventDefault();
   false4.text(`That is incorrect. A study found that over 300 million tons (over 600 billion pounds) of plastic was thrown out in 2016 alone. And it looks like plastic production is not slowing down anytime soon. Although some brands are taking action against one use plastic, they are still using plastic, and a lot of companies continue to use one use plastic.`);
+  truth4.toggleClass(`red`);
 }
 
 myth4.on("click", true5);
@@ -88,4 +97,5 @@ myth4.on("click", true5);
 function true5() {
   event.preventDefault();
   false4.text(`That is correct. A study found that over 300 million tons (over 600 billion pounds) of plastic was thrown out in 2016 alone. And it looks like plastic production is not slowing down anytime soon. Although some brands are taking action against one use plastic, they are still using plastic, and a lot of companies continue to use one use plastic.`);
+  myth4.toggleClass(`green`);
 }
